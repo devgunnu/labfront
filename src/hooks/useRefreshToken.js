88f1598807905c4dd9ec5dd2cc97aaaa8e96a74d
Aaditya Jindal <74290459+RETR0-OS@users.noreply.github.com>
2024-10-26 17:03:25 -0700

    Update useRefreshToken.js

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -5,7 +5,7 @@ const useRefreshToken = () => {
     const { setAuth } = useAuth();
 
     const refresh = async () => {
-        const response = await axios.get('/refresh', {
+        const response = await axios.get('/accounts/api/token/refresh', {
             withCredentials: true
         });
         setAuth(prev => {

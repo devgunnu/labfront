994511e42fb6ed30e06aa719159d745a4de6099d
Gunbir Singh <139300251+devgunnu@users.noreply.github.com>
2024-10-27 06:43:48 +0530

    Update useLogout.js

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -7,7 +7,7 @@ const useLogout = () => {
     const logout = async () => {
         setAuth({});
         try {
-            const response = await axios('/logout', {
+            const response = await axios('/accounts/logout', {
                 withCredentials: true
             });
         } catch (err) {
